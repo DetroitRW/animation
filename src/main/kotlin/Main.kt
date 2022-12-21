505ab624ba2c2faa62632f5eb09d89e2acfbdead
Ilya Brant <detroitib31@gmail.com>
2022-12-21 18:27:27 +0600

    Написал функцию display и render

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,17 @@
 fun main(args: Array<String>) {
+fun render(n: Int): String {
+    val width = 40
+    val height = 80
+    val screen = StringBuilder()
+    for (y in 0 until width) {
+        for (x in 0 until height) {
+            screen.append(".")
+        }
+        screen.append("\n")
+    }
+    return screen.toString()
+}
 
+fun display(screen: String) {
+    println(screen)
 }

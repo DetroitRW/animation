6711fac064506c17b608ef8c19e621f766cc2fb1
Ilya Brant <detroitib31@gmail.com>
2022-12-22 20:24:53 +0600

    Добавил границы

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,17 +6,33 @@ fun main(args: Array<String>) {
         val screen = render(n)
         display(screen)
         n += 1
-        Thread.sleep(300)
+        Thread.sleep(14)
     }
 }
 
 fun render(n: Int): String {
-    val width = 12
-    val height = 6
+    val width = 80
+    val height = 40
     val screen = StringBuilder()
     for (y in 0 until height) {
         for (x in 0 until width) {
-            if (x == n % width && y == (n / width) % height) {
+            if (x == 0 && y == 0) {
+                screen.append("┌")
+            } else if (x == 0 && y == height-1) {
+                screen.append("└")
+            } else if (x == width - 1 && y == 0) {
+                screen.append("┐")
+            } else if (x == width - 1 && y == height - 1) {
+                screen.append("┘")
+            } else if (x == width - 1 && y in 1 until height - 1) {
+                screen.append("│")
+            } else if (x == 0 && y in 1 until height - 1) {
+                screen.append("│")
+            } else if (y == 0) {
+                screen.append("─")
+            } else if (y == height - 1) {
+                screen.append("─")
+            } else if (x == n % width && y == (n / width) % height) {
                 screen.append("*")
             } else {
                 screen.append(".")
@@ -30,3 +46,4 @@ fun render(n: Int): String {
 fun display(screen: String) {
     println(screen)
 }
+//─, │, ┌, ┐, └, ┘

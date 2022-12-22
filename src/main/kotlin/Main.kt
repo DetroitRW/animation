78d747ac1f26855d0cc2f2d6b1a6cb2319249f91
Ilya Brant <detroitib31@gmail.com>
2022-12-22 14:34:16 +0600

    Задал звезде движение по горизонтали

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,17 +6,21 @@ fun main(args: Array<String>) {
         val screen = render(n)
         display(screen)
         n += 1
-        Thread.sleep(14)
+        Thread.sleep(300)
     }
 }
 
 fun render(n: Int): String {
-    val width = 40
-    val height = 80
+    val width = 12
+    val height = 6
     val screen = StringBuilder()
-    for (y in 0 until width) {
-        for (x in 0 until height) {
-            screen.append(".")
+    for (y in 0 until height) {
+        for (x in 0 until width) {
+            if (x == n % width && y == 0) {
+                screen.append("*")
+            } else {
+                screen.append(".")
+            }
         }
         screen.append("\n")
     }
